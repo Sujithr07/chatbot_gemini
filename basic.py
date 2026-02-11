2a545cb8e6307ca0e8aea9abca5b44bbeb414b21
Sujith <sujithj7123@gmail.com>
2026-02-11 22:27:47 +0530

    sys instruc and temp

--- basic.py
+++ basic.py
@@ -1,12 +1,16 @@
 from google import genai
-
+from google.genai import types
 
 client = genai.Client()
 prompt = input("enter your prompt : ")
 
 response = client.models.generate_content(
     model= 'gemini-2.5-flash',
-    contents= prompt
+    contents= prompt,
+    config= types.GenerateContentConfig(
+        system_instruction= "response must be in 25 words",
+        temperature= 0.1
+    )
 )
 
 print(response.text)
